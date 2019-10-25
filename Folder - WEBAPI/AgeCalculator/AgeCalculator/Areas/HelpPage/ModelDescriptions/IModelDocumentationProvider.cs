ea4458d36ba17197367e881dbbd8e9c6e0a3e229
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-10-25 17:15:24 -0300

    Commit creating API for IMC calc

---
+++ Folder - WEBAPI/AgeCalculator/AgeCalculator/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace AgeCalculator.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ Folder - WEBAPI/AgeCalculator/AgeCalculator/Controllers/AgeCalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AgeCalculator.Controllers
+{
+    public class AgeCalculatorController : ApiController
+    {
+        public string Get()
+        {
+            return "App to drink tonight";
+        }
+
+        public string Get(int birthYear,string userName="User")
+        {
+            if ((DateTime.Now.Year - birthYear) >= 18)
+            {
+                return $"{userName}, according to the system, you may drink alcoolichs drinks" ;
+            }
+            else
+            {
+                return "{userName}, according to the system, you may drink juice or milk";
+            }
+        }
+
+    }
+}
