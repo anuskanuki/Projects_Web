ea0f848b07786d97653f7d9a10b304d5b2a7d357
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-10-28 11:07:31 -0300

    Commit w/ local web API, changes, improvements and complete results

---
+++ Folder - WEBAPI/IMC_Website/IMC_Website/Scripts/Script.js
@@ -0,0 +1,29 @@
+﻿
+$('#imc').mask("##.00");
+
+$(document).ready(function () {
+    $('#show').click(function () {
+        var formCampos = $('#form_principal').serialize();
+
+        $.get("http://localhost:59189/api/Imc?" + formCampos, function (data) {
+            $('#imc').val(data.ImcValue);
+        });
+
+        $.get("http://localhost:59189/api/Imc/Msg?" + formCampos, function (data) {
+            $('#msg').val(data)
+        });
+    });
+
+    $('#clear').click(function () {
+        ClearScreen();
+    });
+
+});
+
+function ClearScreen() {
+    var myInputs = $('input[type="text"]');
+    myInputs.val("");
+}
+
+
+
