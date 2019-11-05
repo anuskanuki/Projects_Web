b4760b8793619c145d2eccacc7bdf47343f9a95d
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-11-05 17:25:21 -0200

    Commit finalizations

---
+++ Folder - WEBAPI/LoginUsuarios/LoginUsuarios/Models/UsuarioContextDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginUsuarios.Models
+{
+    public class UsuarioContextDB : DbContext
+    {
+        public UsuarioContextDB(DbConfiguration<>)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/JS/script.js
@@ -0,0 +1,47 @@
+﻿var Pessoa =
+{
+    Login: "Nuki",
+    Email:"nuki@gmail.com",
+    Senha: "123"
+}
+
+var ArrayPessoas = [Pessoa];
+
+$(document).ready(
+    function () {
+        $('#mostrar').click(function () {
+
+            var stringTemplate = "Pessoas Registradas:\n\n";
+
+            for (var i = 0; i < ArrayPessoas.length; i++) {
+                stringTemplate += ArrayPessoas[i].Login + "\r\n";
+            }
+
+            alert(stringTemplate);
+            ClearScreen();
+        });
+
+        $('#logar').click(function () {
+            //var NewPessoa =
+            //{
+            //    Nome: $('#nome').val(),
+            //    Login: $('#login').val()
+            //}
+
+            //ArrayPessoas.push(NewPessoa);
+            //var stringResult = "Pessoa cadastrada com sucesso!\r\nName: "
+            //    + NewPessoa.Name + "\r\nBreed: " + NewPessoa.Breed +
+            //    "\r\nColor: " + NewPessoa.Color + "\r\n" + NewPessoa.Age + " years";
+            //alert(stringResult);
+            //ClearScreen();
+            alert("BEM-VINDO!");
+        });
+
+    });
+function ClearScreen() {
+    var myInputs = $('input[type="text"]');
+    myInputs.val("");
+}
+
+
+
